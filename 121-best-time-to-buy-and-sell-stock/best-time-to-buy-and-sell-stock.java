d07b04399f71a48bccf21176a3d57cdd63b2c1a5
Denis <denismaheshwari@gmail.com>
2023-07-19 17:22:53 +0530

    Time: 998 ms (6.59%) | Memory: 57.4 MB (95.18%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -10,9 +10,11 @@ class Solution {
             while(j<prices.length){
                 if(prices[j]-prices[i]>max){
                     max=prices[j]-prices[i];
+                    if(prices[i]==newPrices[0] && prices[i]==newPrices[prices.length-1])return max;
                 } 
                 ++j;
-                if(prices[i]==newPrices[0] && j==prices.length)return max;      
+                if(prices[i]==newPrices[0] && j==prices.length)return max;
+                      
 
             }
         }
