cbdba4d4de4757378c8e6676d08a3db79b7971c3
Denis <denissmaheshwari@gmail.com>
2023-07-22 12:15:52 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (12.82%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -3,14 +3,9 @@ class Solution {
         
         int red=0, white=0,blue=0;
         for(int i=0;i<nums.length;i++){
-            
-           
-            if(nums[i]==0)++red;
+           if(nums[i]==0)++red;
             else if(nums[i]==1)++white;
             else if(nums[i]==2)++blue;
-            
-            
-
         }
         int w=white;
         for(int i=0;i<nums.length;i++){
@@ -19,16 +14,6 @@ class Solution {
                 nums[i]=1;
             }
             else nums[i]=2;
-
         }
-        
-
     }
-    public void swap(int a, int b){
-        int c=0;
-        c=a;
-        a=b;
-        b=c;
-    }
-
-}
+ }
