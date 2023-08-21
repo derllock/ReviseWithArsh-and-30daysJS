796648975eb0cc941e9b8567ba7aca7739a2aadb
Denis <denissmaheshwari@gmail.com>
2023-08-21 11:51:15 +0530

    Update find-all-duplicates-in-an-array.java

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -14,11 +14,11 @@ class Solution {
 //     public List<Integer> findDuplicates(int[] nums) {
 //         List<Integer> ans = new ArrayList<>();
 
-//         for (int num : nums) {
-//             if (nums[Math.abs(num) - 1] < 0) { // seen before
+//         for (int num : nums) {                                                   // fundamental find duplicates in array in O(N) works because every value in array is less than length of array
+//             if (nums[Math.abs(num) - 1] < 0) { // seen before                      
 //                 ans.add(Math.abs(num));
 //             }
-//             nums[Math.abs(num) - 1] *= -1;
+//             nums[Math.abs(num) - 1] *= -1;                                        // just convert that index value to minus ewhich is visited 
 //         }
 
 //         return ans;
