441ba359652e48e87dc07815a26342d7fdfab916
Denis <denissmaheshwari@gmail.com>
2023-08-25 20:51:30 +0530

    Time: 3 ms (14.64%) | Memory: 40 MB (98.72%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,15 +2,17 @@ class Solution {
     public boolean isValid(String s){
         int equal=0,open=0,bracket=0;
         StringBuilder string=new StringBuilder(s);
-        if(string.charAt(0)==')' || string.charAt(0)=='}' || string.charAt(0)==']') return false;
+        
         for(int i=1; i<string.length();i++){
+            if(string.charAt(0)==')' || string.charAt(0)=='}' || string.charAt(0)==']') return false;
             if((string.charAt(i)==')' && string.charAt(i-1)=='(') ||
                         (string.charAt(i)=='}'&& string.charAt(i-1)=='{') ||
                         (string.charAt(i)==']' && string.charAt(i-1)=='[') ){
                             string.deleteCharAt(i);
                             string.deleteCharAt(i-1);
-                            i=0;
+                            i=i-2;
                     }
+                    if(string.length()==1)return false;
                 
                 
         }
