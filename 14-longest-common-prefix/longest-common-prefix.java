98a49c81931d012ea9697bb18f773e86a386d0d6
Denis <denissmaheshwari@gmail.com>
2023-08-26 17:51:35 +0530

    Time: 2 ms (29.58%) | Memory: 42.4 MB (6.59%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -22,7 +22,18 @@ class Solution {
         finalAns+=subs;
     }
     return finalAns;
-            
+        //     Arrays.sort(strs);                    foundation: sort on string array arranged lexicographically
+        // String s1 = strs[0];
+        // String s2 = strs[strs.length-1];                  
+        // int idx = 0;
+        // while(idx < s1.length() && idx < s2.length()){
+        //     if(s1.charAt(idx) == s2.charAt(idx)){
+        //         idx++;
+        //     } else {
+        //         break;
+        //     }
+        // }
+        // return s1.substring(0, idx);
            
     }
 }
