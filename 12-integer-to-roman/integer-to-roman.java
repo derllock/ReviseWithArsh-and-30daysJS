efac938bc215134589a2cf5b144df4332a495ff1
Denis <denissmaheshwari@gmail.com>
2023-08-28 20:57:34 +0530

    Time: 14 ms (21.94%) | Memory: 43.8 MB (32.79%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,69 +1,76 @@
 class Solution {
     public String intToRoman(int num) {
-        String roman="";
-        int nums=num;
+        // String roman="";
+        // int nums=num;
         
-        int pow=0;
-        while(pow<String.valueOf(num).length()){
-            int digit=(nums%10);
-            ++pow;
-            nums=nums/10;
-            if(pow==1){
-                switch(digit){
-                    case 1:roman="I"; break;
-                    case 2:roman="II";break;
-                    case 3:roman="III"; break;
-                    case 4:roman="IV"; break;
-                    case 5:roman="V" ;break;
-                    case 6:roman="VI" ;break;
-                    case 7:roman="VII" ;break;
-                    case 8:roman="VIII" ;break;
-                    case 9:roman="IX"; break;
-                    default: break;
-                }
-            }
-            else if(pow==2){
-               switch(digit){
-                    case 1:roman="X"+roman; break;
-                    case 2:roman="XX"+roman; break;
-                    case 3:roman="XXX"+roman; break;
-                    case 4:roman="XL"+roman; break;
-                    case 5:roman="L"+roman;break;
-                    case 6:roman="LX"+roman; break;
-                    case 7:roman="LXX" +roman;break;
-                    case 8:roman="LXXX" +roman;break;
-                    case 9:roman="XC" +roman;break;
-                    default: break;
-                } 
-            }
-            else if(pow==3){
-               switch(digit){
-                    case 1:roman="C"+roman; break;
-                    case 2:roman="CC"+roman; break;
-                    case 3:roman="CCC"+roman; break;
-                    case 4:roman="CD"+roman; break;
-                    case 5:roman="D"+roman;break;
-                    case 6:roman="DC"+roman; break;
-                    case 7:roman="DCC" +roman;break;
-                    case 8:roman="DCCC" +roman;break;
-                    case 9:roman="CM" +roman;break;
-                    default: break;
-                } 
-            }
+        // int pow=0;
+        // while(pow<String.valueOf(num).length()){
+        //     int digit=(nums%10);
+        //     ++pow;
+        //     nums=nums/10;
+        //     if(pow==1){
+        //         switch(digit){
+        //             case 1:roman="I"; break;
+        //             case 2:roman="II";break;
+        //             case 3:roman="III"; break;
+        //             case 4:roman="IV"; break;
+        //             case 5:roman="V" ;break;
+        //             case 6:roman="VI" ;break;
+        //             case 7:roman="VII" ;break;
+        //             case 8:roman="VIII" ;break;
+        //             case 9:roman="IX"; break;
+        //             default: break;
+        //         }
+        //     }
+        //     else if(pow==2){
+        //        switch(digit){
+        //             case 1:roman="X"+roman; break;
+        //             case 2:roman="XX"+roman; break;
+        //             case 3:roman="XXX"+roman; break;
+        //             case 4:roman="XL"+roman; break;
+        //             case 5:roman="L"+roman;break;
+        //             case 6:roman="LX"+roman; break;
+        //             case 7:roman="LXX" +roman;break;
+        //             case 8:roman="LXXX" +roman;break;
+        //             case 9:roman="XC" +roman;break;
+        //             default: break;
+        //         } 
+        //     }
+        //     else if(pow==3){
+        //        switch(digit){
+        //             case 1:roman="C"+roman; break;
+        //             case 2:roman="CC"+roman; break;
+        //             case 3:roman="CCC"+roman; break;
+        //             case 4:roman="CD"+roman; break;
+        //             case 5:roman="D"+roman;break;
+        //             case 6:roman="DC"+roman; break;
+        //             case 7:roman="DCC" +roman;break;
+        //             case 8:roman="DCCC" +roman;break;
+        //             case 9:roman="CM" +roman;break;
+        //             default: break;
+        //         } 
+        //     }
             
-            else if(pow==4){
-               switch(digit){
-                    case 1:roman="M"+roman; break;
-                    case 2:roman="MM"+roman; break;
-                    case 3:roman="MMM"+roman; break;
-                     default: break;
-                } 
-            }
-
+        //     else if(pow==4){
+        //        switch(digit){
+        //             case 1:roman="M"+roman; break;
+        //             case 2:roman="MM"+roman; break;
+        //             case 3:roman="MMM"+roman; break;
+        //              default: break;
+        //         } 
+        //     }
+ String ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String ths[]={"","M","MM","MMM"};
+        
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+    
 
             
-        }
-        return roman;
+        // }
+        // return roman;
+        
 
         
     }
