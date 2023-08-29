c441a4880b65d1c8c86b00f5578635384af4bb24
Denis <denissmaheshwari@gmail.com>
2023-08-29 18:58:09 +0530

    Time: 9 ms (45.84%) | Memory: 44.6 MB (31.60%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -10,8 +10,8 @@ class Solution {
         while(index<sb.length()){
             if(sb.charAt(index)!=sbreverse.charAt(index)){
                 
-                if(sb.deleteCharAt(index).compareTo(sbreverse.deleteCharAt(sbreverse.length()-1-index))==0)return true;
-                else if(sb2.deleteCharAt(sb2.length()-1-index).compareTo(sbreverse2.deleteCharAt(index))==0)return true;
+                if(sb.deleteCharAt(index).compareTo(sbreverse.deleteCharAt(sbreverse.length()-1-index))==0)return true; // for test case:cbcc if we 
+                else if(sb2.deleteCharAt(sb2.length()-1-index).compareTo(sbreverse2.deleteCharAt(index))==0)return true;// encounter wrong char we have check both permutations
                 else return false;
                 }
             
