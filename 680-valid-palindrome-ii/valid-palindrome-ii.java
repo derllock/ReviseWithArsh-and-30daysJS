03d548467e3d8bab8bc06aaf7f7364f0cdb0a095
Denis <denissmaheshwari@gmail.com>
2023-08-29 18:58:33 +0530

    Time: 9 ms (45.84%) | Memory: 44.3 MB (75.46%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -10,7 +10,7 @@ class Solution {
         while(index<sb.length()){
             if(sb.charAt(index)!=sbreverse.charAt(index)){
                 
-                if(sb.deleteCharAt(index).compareTo(sbreverse.deleteCharAt(sbreverse.length()-1-index))==0)return true; // for test case:cbcc if we 
+                if(sb.deleteCharAt(index).compareTo(sbreverse.deleteCharAt(sbreverse.length()-1-index))==0)return true; //foundation StringBuilder  for test case:cbcc if we 
                 else if(sb2.deleteCharAt(sb2.length()-1-index).compareTo(sbreverse2.deleteCharAt(index))==0)return true;// encounter wrong char we have check both permutations
                 else return false;
                 }
