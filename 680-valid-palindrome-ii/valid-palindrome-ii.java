3f5c47a0bfc51363f66b9dd919231efa2b989eff
Denis <denissmaheshwari@gmail.com>
2023-08-29 18:29:43 +0530

    Time: 15 ms (8.17%) | Memory: 44.4 MB (45.38%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -4,10 +4,11 @@ class Solution {
         sbreverse.reverse();
         StringBuilder sb=new StringBuilder(s);
         int index=0;
+        StringBuilder sb2=new StringBuilder(sb);
+         StringBuilder sbreverse2=new StringBuilder(sbreverse);
         while(index<sb.length()){
             if(sb.charAt(index)!=sbreverse.charAt(index)){
-                StringBuilder sb2=new StringBuilder(sb);
-                StringBuilder sbreverse2=new StringBuilder(sbreverse);
+                
                 if(sb.deleteCharAt(index).compareTo(sbreverse.deleteCharAt(sbreverse.length()-1-index))==0)return true;
                 else if(sb2.deleteCharAt(sb2.length()-1-index).compareTo(sbreverse2.deleteCharAt(index))==0)return true;
                 else return false;
