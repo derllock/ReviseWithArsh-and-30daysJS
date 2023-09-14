11eb1f74da810bdae0284f9bfe2e9bbd5500f79b
Denis <denissmaheshwari@gmail.com>
2023-09-14 18:23:30 +0530

    Time: 48 ms (71.56%) | Memory: 41.6 MB (64.61%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+// var createHelloWorld = function() {
+//     return function(...args) {
+//        return "Hello World";
+//     }
+// };
+
+
+//  const f = createHelloWorld();
+//   f(); // "Hello World"
+
+const createHelloWorld=(...args)=>{
+  return helloWorld=(...args)=>{
+    return"Hello World";
+  }
+}
+ 
