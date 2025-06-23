c0bdfc6d073c771592e0f67354bde2bbca28f563
Denis <denissmaheshwari@gmail.com>
2025-06-23 12:40:26 +0530

    Time: 27 ms (5.72%) | Memory: 44.4 MB (27.13%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)return false;
+        else{
+            int ddend=x;
+            int remainder=0;
+            while(ddend>0){
+                remainder=remainder*10+ddend%10;
+                ddend/=10;
+            }
+            System.out.println(remainder);
+            if(remainder==x)return true;
+            else return false;
+        }
+    }
+}
